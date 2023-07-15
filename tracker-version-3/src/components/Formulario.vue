dfdf425b7fdca5b692ae8c29f0e1c6c19edec456
rodneyazev <rodney.n.azevedo@gmail.com>
2023-07-14 21:23:58 -0300

    Cronometro refactoring

---
+++ tracker-version-3/src/components/Formulario.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="box">
+        <div class="columns">
+            <div class="column is-8" role="form" area-label="Formulário para criação de uma nova tarefa">
+                <input type="text" class="input" placeholder=" Qual tarefa você deseja iniciar?">
+            </div>
+            <div class="column">
+                <div class="is-flex is-align-items-center is-justify-content-space-between">
+                    <Cronometro :tempoEmSegundos="tempoEmSegundos" />
+                    <button class="button" @click="iniciarContagem">
+                        <span class="icon">
+                            <i class="fas fa-play"></i>
+                        </span>
+                        <span>play</span>
+                    </button>
+                    <button class="button" @click="finalizarContagem">
+                        <span class="icon">
+                            <i class="fas fa-stop"></i>
+                        </span>
+                        <span>stop</span>
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+    import { defineComponent } from 'vue';
+    import Cronometro from './Cronometro.vue';
+
+    export default defineComponent({
+        name: 'Formulario',
+        components: {
+            Cronometro
+        },
+        data() {
+            return {
+                tempoEmSegundos: 0,
+                cronometro: 0
+            }
+        },
+        methods: {
+            iniciarContagem() {
+                // 1 seg = 1000 ms
+                this.cronometro = setInterval(()=> {
+                    this.tempoEmSegundos += 1;
+                }, 1000)
+            },
+            finalizarContagem(){   
+                clearInterval(this.cronometro);
+            }
+        }
+    });
+
+</script>
+
+<style scoped></style>
