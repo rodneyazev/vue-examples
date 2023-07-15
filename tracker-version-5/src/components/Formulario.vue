0a78e8dfd3e027286524b5da5719b1966b34ddd9
rodneyazev <rodney.n.azevedo@gmail.com>
2023-07-14 21:38:36 -0300

    Enable and Disable Play and Stop Button

---
+++ tracker-version-5/src/components/Formulario.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="box">
+        <div class="columns">
+            <div class="column is-8" role="form" area-label="Formulário para criação de uma nova tarefa">
+                <input type="text" class="input" placeholder=" Qual tarefa você deseja iniciar?">
+            </div>
+            <div class="column">
+                <Temporizador />
+            </div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+
+    import { defineComponent } from 'vue';
+    import Temporizador from './Temporizador.vue';
+
+    export default defineComponent({
+        name: 'Formulario',
+        components: {
+            Temporizador
+        }
+    });
+
+</script>
+
+<style scoped></style>
